77982e5f0e41642ecbb4a6b4ba48b7b0b75b4572
Johan Kreivi <johan.kreivi@gmail.com>
2022-10-28 12:58:37 +0200

    Final before compleeting asignment. if-statments is removed inside while loop switch-case added!

--- Calc.cs
+++ Calc.cs
@@ -1,33 +1,175 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Data;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace CalculatorNamespace
 {
+    /// <summary>
+    /// Hello this is **Calc** from *CalculatorNamespace*
+    /// </summary>
     public class Calc
     {
         private List<string> _historyList = new List<string>();
 
-        public Calc(string hl = "History:")
+        /// <summary>
+        /// This is a constructor for Calc class
+        /// A primary input for HistoryList is added wich fuction as a header. 
+        /// </summary>
+        public Calc()
         {
-            _historyList.Add(hl);
+            HistorList.Add("History:");
         }
 
-        public List<string> historyList
+        /// <summary>
+        /// A public get/set for _historylist
+        /// </summary>
+        public List<string> HistorList
         {
             get => _historyList;
             set => _historyList = value;
         }
 
+        /// <summary>
+        /// Compute Method takes one input and calculates provided expression with the help from System.Data.Datatable.
+        /// The assignment did not exclude usage of imported libraries :) Thereby more time spent on Exception handling and these are logged! See Log()
+        /// The result is printed to console together with the initial expression provided.
+        /// </summary>
+        /// <param name="input">A string containing expression for computation</param>
+        /// <exception cref="DivideByZeroException">
+        ///     As a part of the assignment, when zero division is found, this exception is thrown. 
+        ///     Result from DataTable.Compute equal to "infinity" is checked because no actual zero division exception is thrown by Cumpute.
+        /// </exception>
+        internal void Compute(string? input)
+        {
+            DataTable dt = new();
+            var v = dt.Compute(input, "2");
+
+            //Checking for zero division by comparing result to infinity symbol, stored as unicode value in infinity.
+            string infinity = "\u221E";
+            if (v.ToString().Equals(infinity))
+            {
+                throw new DivideByZeroException();
+            }
+            //     Add results to the list
+            HistorList.Add($"{input} = {v}");
+            HistoryLog($"{input} = {v}");
+            Console.WriteLine($"{input} = {v}");
+        }
+
+        /// <summary>
+        /// GetHistory() prints previous calculations, if there are any...
+        /// </summary>
+        internal void GetHistory()
+        {
+
+            foreach (var item in HistorList)
+            {
+                Console.WriteLine(item);
+            }
+            if (HistorList.Count < 2)
+            {
+                Console.WriteLine("Make some calculations to fill the history!");
+            }
+        }
+
+        /// <summary>
+        /// Head() is simply outputting a nice headline for the best calculator :)
+        /// </summary>
         internal void Head()
         {
-            // Välkomnande meddelande
             Console.WriteLine("Hi! Welcome to the best calculator in the world");
-            Console.WriteLine("----- [H] : History ----- [exit] : exit -----");
-            Console.WriteLine("You can use: + - / % ");
+            Console.WriteLine("----- [h/H] : History - [fh/FH] : FullHistory - [e/E] : exit -----");
+            Console.WriteLine("Valid operators and syntax include: + - / % () ");
+        }
+
+        /// <summary>
+        /// Log() is writing any exceptions to the BestCalculatorLog.txt file.
+        /// Date and time is also provided.
+        /// </summary>
+        /// <param name="textToLog">A string for logging</param>
+        internal void Log(string textToLog)
+        {
+            string path = @"BestCalculatorLog.txt";
+            // This text is added only once to the file.
+            if (!File.Exists(path))
+            {
+                // Create a file to write to. And a header.
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine("This is a log for the best calculator in the world: ");
+                }
+            }
+
+            // This text is always added, making the file longer over time
+            // if it is not deleted...
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                string date = DateTime.Now.ToString();
+                sw.WriteLine($"--- Logging START {date} ---");
+                sw.WriteLine(textToLog);
+                sw.WriteLine($"--- Logging END {date} ---");
+            }
+        }
+
+        /// <summary>
+        /// Logging of history by HistoryLog(), in reverse order with current datetime.
+        /// </summary>
+        /// <param name="textToLog">A string for logging</param>
+        internal void HistoryLog(string textToLog)
+        {
+            string path = @"HistoryLog.txt";
+
+            //Create file in path to avoid exception.
+            if (!File.Exists(path))
+            {
+                StreamWriter sw = File.CreateText(path);
+                sw.Close();
+            }
+
+            //TODO: Reverse printing to file! DONE!
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+            {
+                string date = DateTime.Now.ToString();
+                StreamReader sr = new StreamReader(fs);
+                string old = sr.ReadToEnd();
+                textToLog =  date + ": " + textToLog + "\n" + old;
+                byte[] byteStream = Encoding.Default.GetBytes(textToLog);
+                fs.Seek(0, SeekOrigin.Begin);
+                fs.Write(byteStream, 0, byteStream.Length);
+                fs.Close();
+            }
+        }
+
+        /// <summary>
+        /// GetHistoryLog() outputs a 20 line long list of historical calcolations to console.
+        /// </summary>
+        internal void GetHistoryLog()
+        {
+            string path = @"HistoryLog.txt";
+            // This text is opened only if it exists...
+            if (File.Exists(path))
+            {
+                Console.WriteLine("20 of the latest calculations... Latest first. Complete List can be found in HistoryLog.txt");
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    for (int i = 0; i < 20; i++)
+                    {
+                        string? line = sr.ReadLine();
+                        if (line != null)
+                        {
+                            Console.WriteLine(line);
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                //if no historical calculation-file exists... 
+                Console.WriteLine("Historical calculations do not exist!");
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,73 +1,132 @@
-﻿using CalculatorNamespace;
-using System;
-using System.Collections;
-using System.Data;
-using System.Runtime.CompilerServices;
+﻿using System.Data;
 
 namespace CalculatorNamespace;
 /// <summary>
+/// This program is a console based calculator wich is the final exam of my "programming basics" course.
+/// The Pseudo code for the assignment are as follows: (also provided in code for easy nav.)
+/// 
+///     Welcome message
+///     A list to save history of calculations
+///     The user inputs number and math operation
+///     NOTE! The user must enter a number in order to move forward in the program!
+///     If the user were to divide 0 by 0 display Invalid input!
+///     Add results to the list
+///     Show result
+///     Ask user if it wants to show previous results.
+///     Display previous results
+///     Ask the user whether to exit or continue.
+///
+/// As extra features:
+/// -XML comments are used, this makes it easy to provide a online code/API doc in the future. See: https://dotnet.github.io/docfx/
+/// -Logging of exceptions to BestCalculatorLog.txt in bin folder
+/// -Logging of all calculations to HistoryLog.txt in bin folder.
+/// -A Calculator class is used to keep the code cleaner. incuding theese methods:
+///     Calculator.HistorList()
+///     Calculator.Compute()
+///     Calculator.Log()
+///     Calculator.HistoryLog()
+///     Calculator.GetHistoryLog()
+///     And more... see calc.cs for full documentation.
+/// 
+/// Further development includes:
+/// TODO: A nicer presentation, including alignment of text
+/// TODO: Implement a more graphical menu, like this sweet one: https://github.com/lorenarms/Personal_CSharp_Utilities/tree/master/MenuBuilder
 /// 
 /// </summary>
 internal class Program
 {
     private static void Main(string[] args)
     {
-        bool exit = false;
-        Calc Calculator = new Calc();
 
+        bool exit = false;
+        var Calculator = new Calc();
+
+        //     Welcome message
+        //     Ask user if it wants to show previous results. "h" or "hl"
+        //     Ask the user whether to exit or continue. "e"
         Calculator.Head();
-        string input = Console.ReadLine();
+
+        //     The user inputs number and math operation
+        string? input = Console.ReadLine();
 
         do
         {
-            if (input.ToLower() == "exit")
-            {
-                break;
-            }
-            Console.Clear();
-            Calculator.Head();
-            if (input.ToLower() == "h")
-            {
-                Console.Clear();
-                Calculator.Head();
-                foreach (var item in Calculator.historyList)
-                {
-                    Console.WriteLine(item);
-                }
-            }
-            else
-            {
-                try
-                {
-                    if (input != "")
-                    {
-                        DataTable dt = new DataTable();
-                        var v = dt.Compute(input, "2");
-                        Calculator.historyList.Add($"{input} = {v}");
-                        Console.WriteLine($"{input} = {v}");
-                    }
-                }
-                catch (EvaluateException)
-                {
-                    Console.WriteLine("You can't do that! Use numbers and operators or allowed text for navigation.");
-                }
-                catch (OverflowException ex) { Console.WriteLine("Too large number!"); }
+            /*
+             For easy to read purpose, a switch case approch is used. previous versions was made up by if-else statments.
+             A do-while loop is used to keep the calculator going. Aslong as an "e" is not provided.
+             */
 
+            //A clean console is the way to go! 
+            Console.Clear();
+            //New header is printed
+            Calculator.Head();
+
+            switch (input?.ToLower())
+            {
+                //     Ask the user whether to exit or continue.
+                case "e":
+                    Environment.Exit(0);
+                    break;
+                //     Display previous results, see h and hl.
+                case "h":
+                    Console.Clear();
+                    Calculator.Head();
+                    Calculator.GetHistory();
+                    break;
+                case "hl":
+                    Calculator.GetHistoryLog();
+                    break;
+
+
+                //     NOTE! The user must enter a number in order to move forward in the program! This is checked inside the try-catch...
+                default:
+                    try
+                    {
+                        if (input != "")
+                        {
+
+                            //     A list to save history of calculations is created within compute()
+                            //     If the user were to divide 0 by 0 display Invalid input!
+                            //     Show result
+                            //     see inside Compute() for more info.
+                            Calculator.Compute(input);
+                        }
+                    }
+                    catch (EvaluateException ex)
+                    {
+                        Console.WriteLine("You can't do that! Use numbers and mathoperators or allowed text for navigation.");
+                        Console.WriteLine("Try again:");
+                        Calculator.Log(ex.ToString());
+                    }
+                    catch (OverflowException ex)
+                    {
+                        Console.WriteLine("Too large number!");
+                        Console.WriteLine("Try again:");
+                        Calculator.Log(ex.ToString());
+                    }
+                    catch (SyntaxErrorException ex)
+                    {
+                        Console.WriteLine("Missing operand after operator.");
+                        Console.WriteLine("Try again:");
+                        Calculator.Log(ex.ToString());
+                    }
+                    catch (DivideByZeroException ex)
+                    {
+                        Console.WriteLine("Invalid input! You´re getting close to infinity, be careful!");
+                        Calculator.Log(ex.ToString());
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                        Console.WriteLine("Try again:");
+                        Calculator.Log(ex.ToString());
+                    }
+                    break;
             }
+
+            //     The user inputs number and math operation... this input acts like a pause before while is looping agian.
             input = Console.ReadLine();
-            continue;
+
         } while (!exit);
     }
 }
-
-
-// En lista för att spara historik för räkningar
-// Användaren matar in tal och matematiska operation
-//OBS! Användaren måsta mata in ett tal för att kunna ta sig vidare i programmet!
-// Ifall användaren skulle dela 0 med 0 visa Ogiltig inmatning!
-// Lägga resultat till listan
-//Visa resultat
-//Fråga användaren om den vill visa tidigare resultat.
-//Visa tidigare resultat
-//Fråga användaren om den vill avsluta eller fortsätta.
-
